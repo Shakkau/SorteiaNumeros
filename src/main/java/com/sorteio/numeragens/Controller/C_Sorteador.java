1b2f7466489cb99a4b5d4d13ad1545f2e954be55
Shakkau <Anderson_Junior2@hotmail.com>
2023-09-12 21:46:55 -0300

    commit fazendo segunda página

--- src/main/java/com/sorteio/numeragens/Controller/C_Sorteador.java
+++ src/main/java/com/sorteio/numeragens/Controller/C_Sorteador.java
@@ -2,6 +2,7 @@ package com.sorteio.numeragens.Controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class C_Sorteador {
@@ -14,5 +15,10 @@ public class C_Sorteador {
     @GetMapping("/inicio")
     public String getFirstPage(){
         return "inicPage/firstPage";
+    }
+
+    @PostMapping("/")
+    public String sorteando(){
+        return "";
     }
 }
