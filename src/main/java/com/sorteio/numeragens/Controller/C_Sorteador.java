74cb671156ad1927b5a6f8472d5a940a6ae09057
Shakkau <Anderson_Junior2@hotmail.com>
2023-09-13 11:55:12 -0300

    começando o backend

--- src/main/java/com/sorteio/numeragens/Controller/C_Sorteador.java
+++ src/main/java/com/sorteio/numeragens/Controller/C_Sorteador.java
@@ -3,22 +3,57 @@ package com.sorteio.numeragens.Controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 @Controller
 public class C_Sorteador {
 
     @GetMapping("/")
-    public String pagInicial(){
+    public String pagInicial() {
         return "inicPage/firstPage";
     }
 
     @GetMapping("/inicio")
-    public String getFirstPage(){
+    public String getFirstPage() {
         return "inicPage/firstPage";
     }
 
-    @PostMapping("/")
-    public String sorteando(){
-        return "";
+    @GetMapping("/paginaResultado")
+    public String getResultPage(){
+        return "resultPage/resultpage";
+    }
+
+    @PostMapping("/paginaResultado")
+    @ResponseBody
+    public String drawNumbers(@RequestParam("quantityNumbers") int quantidade,
+                              @RequestParam("minNumber") int primeiroParametro,
+                              @RequestParam("maxNumber") int segundoParametro,
+                              Model model){
+        int menor = Math.min(primeiroParametro, segundoParametro);
+        int maior = Math.max(primeiroParametro, segundoParametro);
+
+        List<Integer> numerosNoIntervalo = IntStream.rangeClosed(menor, maior)
+                .boxed()
+                .collect(Collectors.toList());
+
+        Random random = new Random();
+
+        // Embaralhe a lista
+        for (int i = 0; i < quantidade; i++) {
+            int indiceSorteado = random.nextInt(numerosNoIntervalo.size());
+            int numeroSorteado = numerosNoIntervalo.remove(indiceSorteado);
+            System.out.println("Número sorteado: " + numeroSorteado);
+        }
+
+        model.addAttribute("numerosSorteados", numerosNoIntervalo);
+
+        return "/resultpage";
     }
 }
