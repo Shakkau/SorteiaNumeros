ac0766731618965371acba0c0257710a1ee32b46
Shakkau <Anderson_Junior2@hotmail.com>
2023-09-14 20:07:25 -0300

    tentativa

--- src/main/java/com/sorteio/numeragens/Service/S_Sorteador.java
+++ src/main/java/com/sorteio/numeragens/Service/S_Sorteador.java
@@ -11,20 +11,42 @@ import java.util.stream.IntStream;
 @Service
 public class S_Sorteador {
 
-    public static List<Integer> geraNumeros(int min, int max, int quantidade, boolean checkedOrdenarN, boolean checkedRepetirN){
-        List<Integer> numerosNoIntervalo = IntStream.rangeClosed(min, max)
-                .boxed()
-                .collect(Collectors.toList());
+    public static List<Integer> geraNumeros(int min, int max, int quantidade,
+                                            boolean checkedOrdenarN, boolean checkedRepetirN) {
 
-        Random random = new Random();
 
-        List<Integer> numerosSorteados = new ArrayList<>();
-        for (int i = 0; i < quantidade; i++) {
-            int indiceSorteado = random.nextInt(numerosNoIntervalo.size());
-            int numeroSorteado = numerosNoIntervalo.remove(indiceSorteado);
-            numerosSorteados.add(numeroSorteado);
+        if (checkedRepetirN) {
+            List<Integer> numerosSorteados = new ArrayList<>();
+            Random random = new Random();
+
+            for (int i = 0; i < quantidade; i++) {
+                int numeroSorteado = min + random.nextInt(max - min + 1);
+                numerosSorteados.add(numeroSorteado);
+            }
+
+            return numerosSorteados;
+        } else {
+            if (quantidade > max || quantidade < min) {
+                try {
+                    List<Integer> numerosNoIntervalo = IntStream.rangeClosed(min, max)
+                            .boxed()
+                            .collect(Collectors.toList());
+
+                    Random random = new Random();
+
+                    List<Integer> numerosSorteados = new ArrayList<>();
+                    for (int i = 0; i < quantidade; i++) {
+                        int indiceSorteado = random.nextInt(numerosNoIntervalo.size());
+                        int numeroSorteado = numerosNoIntervalo.remove(indiceSorteado);
+                        numerosSorteados.add(numeroSorteado);
+                    }
+                    return numerosSorteados;
+                } catch (RuntimeException exception){
+                    return null;
+                }
+            }
+
         }
-
-        return numerosSorteados;
+        return null;
     }
 }
